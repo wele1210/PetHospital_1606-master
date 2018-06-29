e79673b7a2ff6d673c49fcd006f76f8d83cf7ee2
wele1210 <215831364@qq.com>
2018-06-29 09:50:02 +0800

    社区宠物医院

---
+++ src/ph/servlet/CustomerServlet.java
@@ -0,0 +1,141 @@
+package ph.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ph.dao.PetDAO;
+import ph.dao.UserDAO;
+import ph.po.Pet;
+import ph.po.User;
+
+//@WebServlet(name = "CustomerServlet")
+@WebServlet("/CustomerServlet")
+public class CustomerServlet extends HttpServlet
+{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String m = request.getParameter("m");
+        if("search".equals(m))//查询已有客户，来自于customersearch.jsp提交的表单
+        {
+            searchCustomer(request, response);
+        }
+        else if("save".equals(m))//保存新的客户，来自于customeradd.jsp提交的表单
+        {
+            saveCustomer(request, response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String mode = request.getParameter("mode");//获得传递来的mode参数，只有添加宠物成功后，才会有从PetServlet传递来的mode参数
+
+        if("delete".equals(mode))//如果mode的值等于"delete"，说明请求是来自customerserarch_result.jsp的“删除客户”链接.add by hlzhang 20180122
+        {
+            deleteCustomer(request, response);
+        }
+        else if("detail".equals(mode))//如果mode的值等于"detail"，说明请求是来自customerserarch_result.jsp的“查看客户”链接.add by hlzhang 20180413
+        {
+            showDetail(request, response);
+        }
+    }
+
+    private void searchCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+//            List<User> users = new UserDAO().searchCustomer(request.getParameter("cname"));
+            String cname = request.getParameter("cname");
+            UserDAO userDAO = new UserDAO();
+            List<User> users = userDAO.searchCustomer(cname);
+            if (0 == users.size())
+            {
+                request.setAttribute("msg", "没有找到客户信息");
+                request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+
+            }
+            else
+            {
+                request.setAttribute("users", users);
+                request.getRequestDispatcher("/customersearch_result.jsp").forward(request, response);
+            }
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+        }
+    }
+
+    private void deleteCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            int id = Integer.parseInt(request.getParameter("cid"));
+            UserDAO userDAO = new UserDAO();
+            userDAO.delete(id);
+            request.setAttribute("msg", "删除客户成功");
+            request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+        }
+    }
+
+    private void showDetail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            UserDAO userDAO = new UserDAO();
+            PetDAO petDAO = new PetDAO();
+            int ownerId = Integer.valueOf(request.getParameter("id"));//得到customersearch_resultl.jsp的“查看明细”链接发送来的参数客户id
+            User user = userDAO.getById(ownerId);
+            List<Pet> pets = petDAO.getPetsByOwnerId(ownerId);
+            user.setPets(pets);
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("/customerdetail.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+        }
+    }
+
+    private void saveCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String name = request.getParameter("name");
+        if("".equals(name))//客户姓名不能为空
+        {
+            request.setAttribute("msg", "请输入客户姓名");
+            request.getRequestDispatcher("/customeradd.jsp").forward(request, response);//这里可以直接转发到 customeradd.jsp
+            return;// 调整到customeradd.jsp后，函数直接返回，add by hlzhang, 20180122
+        }
+
+        User user=new User();
+        user.setName(request.getParameter("name"));
+        user.setAddress(request.getParameter("address"));
+        user.setTel(request.getParameter("tel"));
+        user.setRole("customer");
+        user.setPwd("123456");
+
+        try
+        {
+            new UserDAO().save(user);
+            request.setAttribute("msg", "添加客户成功");
+            request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/customersearch.jsp").forward(request, response);
+        }
+    }
+}
---
+++ src/ph/servlet/VisitServlet.java
@@ -0,0 +1,134 @@
+package ph.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.util.List;
+
+import ph.dao.PetDAO;
+import ph.dao.VetDAO;
+import ph.po.Pet;
+import ph.dao.VisitDAO;
+import ph.po.Vet;
+import ph.po.Visit;
+
+//@WebServlet(name = "VisitServlet")
+@WebServlet("/VisitServlet")
+public class VisitServlet extends HttpServlet
+{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String mode = request.getParameter("mode");
+        if("search".equals(mode))
+        {
+            searchPet(request, response);
+        }
+        else if("saveCaseHistory".equals(mode))
+        {
+            saveCaseHistory(request, response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String mode = request.getParameter("mode");
+        if ("showCaseHistory".equals(mode))//浏览病历
+        {
+            showCaseHistory(request, response);
+        }
+        else if ("addCaseHistory".equals(mode))
+        {
+            addCaseHistory(request, response);
+        }
+    }
+
+    private void searchPet(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException
+    {
+        String petName = request.getParameter("petName");
+        String ownerName = request.getParameter("ownerName");
+        try
+        {
+//            List<Pet> pets = new PetDAO().search(petName, "");
+            List<Pet> pets = new PetDAO().search(petName, ownerName);
+            if(0==pets.size())
+            {
+                request.setAttribute("msg", "没有查到宠物信息");
+                request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+            }
+            else
+            {
+                request.setAttribute("pets", pets);
+                request.getRequestDispatcher("/visitSearchResult.jsp").forward(request, response);
+            }
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+    }
+
+    private void showCaseHistory(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            VisitDAO visitDAO = new VisitDAO();
+            List<Visit> visits = visitDAO.getVisitsByPetId(Integer.parseInt(request.getParameter("petId")));
+            request.setAttribute("visits", visits);
+            if (0 == visits.size())
+            {
+                request.setAttribute("msg", "没有找到历史病历");
+            }
+            request.getRequestDispatcher("/showCaseHistory.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+    }
+
+    private void addCaseHistory(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            VetDAO vetDAO = new VetDAO();
+            List<Vet> vets = vetDAO.getAll();
+            request.setAttribute("vets", vets);
+            request.getRequestDispatcher("/addCaseHistory.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+    }
+
+    private void saveCaseHistory(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+    {
+        try
+        {
+            Visit visit = new Visit();
+            visit.setPetId(Integer.parseInt(request.getParameter("pid")));
+            visit.setVetId(Integer.parseInt(request.getParameter("vid")));
+            //缺少visitdate,原因是在visitDAO.save()中用MySQL的CURDATE()函数获取当前日期
+            visit.setDescription(request.getParameter("description"));
+            visit.setTreatment(request.getParameter("treatment"));
+            VisitDAO visitDAO = new VisitDAO();
+            visitDAO.save(visit);
+            String petName = request.getParameter("petName");
+            request.setAttribute("msg", "宠物"+ petName +"的病历添加成功");
+//            response.sendRedirect("CustomerServlet?mode=saveVisit&cid="+request.getParameter("cid"));
+            request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+    }
+}
